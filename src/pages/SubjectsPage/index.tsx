b7a02630ddaf2d1acf34c3564448e0d83a02cdcd
micmaclaynd <101046221+micmaclaynd@users.noreply.github.com>
2024-08-09 19:35:44 +0300

    Add subjects page

---
+++ src/pages/SubjectsPage/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Block from '@/components/Block'
+
+import * as styles from './subjects-page.module.scss'
+
+const SubjectsPage: React.FC = () => {
+    return <Block className={styles.subjectsPage}>SubjectsPage</Block>
+}
+
+export default SubjectsPage
