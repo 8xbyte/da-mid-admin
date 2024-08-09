ab1c895eabf35a74a4e6c3e1176bea5bc1a39ec5
micmaclaynd <101046221+micmaclaynd@users.noreply.github.com>
2024-08-09 19:08:31 +0300

    Update index Add react-router-dom provider

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,13 @@ import App from './App'
 import { createRoot } from 'react-dom/client'
 
 import './index.scss'
+import { BrowserRouter } from 'react-router-dom'
 
 const element = document.getElementById('root') as HTMLElement
 const root = createRoot(element)
 
-root.render(<App />)
+root.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+)
