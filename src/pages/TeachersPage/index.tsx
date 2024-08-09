7e7b750d623a2c58dbdfbb9761472cc2ef733559
micmaclaynd <101046221+micmaclaynd@users.noreply.github.com>
2024-08-09 19:35:56 +0300

    Add teachers page

---
+++ src/pages/TeachersPage/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Block from '@/components/Block'
+
+import * as styles from './teachers-page.module.scss'
+
+const TeachersPage: React.FC = () => {
+    return <Block className={styles.teachersPage}>TeachersPage</Block>
+}
+
+export default TeachersPage
