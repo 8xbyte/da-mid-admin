a4f6a3c9b828e68bd38b585b23d2a5ef43090371
micmaclaynd <101046221+micmaclaynd@users.noreply.github.com>
2024-08-09 19:35:20 +0300

    Add audiences page

---
+++ src/pages/AudiencesPage/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Block from '@/components/Block'
+
+import * as styles from './audiences-page.module.scss'
+
+const AudiencesPage: React.FC = () => {
+    return <Block className={styles.audiencesPage}>AudiencesPage</Block>
+}
+
+export default AudiencesPage
