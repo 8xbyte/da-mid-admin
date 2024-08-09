2f5d2810850d7aa68152c4458eea58909e373a18
micmaclaynd <101046221+micmaclaynd@users.noreply.github.com>
2024-08-09 19:37:01 +0300

    Add classes page

---
+++ src/pages/ClassesPage/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Block from '@/components/Block'
+
+import * as styles from './classes-page.module.scss'
+
+const ClassesPage: React.FC = () => {
+    return <Block className={styles.classesPage}>ClassesPage</Block>
+}
+
+export default ClassesPage
