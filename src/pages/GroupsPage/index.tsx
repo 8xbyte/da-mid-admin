ccb897a22de2348d52fc4b1cc261be5d8717539f
micmaclaynd <101046221+micmaclaynd@users.noreply.github.com>
2024-08-09 19:35:32 +0300

    Add groups page

---
+++ src/pages/GroupsPage/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Block from '@/components/Block'
+
+import * as styles from './groups-page.module.scss'
+
+const GroupsPage: React.FC = () => {
+    return <Block className={styles.groupsPage}>GroupsPage</Block>
+}
+
+export default GroupsPage
