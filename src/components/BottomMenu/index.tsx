d30dd762e1d6c03c12d27cc418f178cbdc9a4681
micmaclaynd <101046221+micmaclaynd@users.noreply.github.com>
2024-08-10 14:50:27 +0300

    Update bottom menu component Update import alias

--- src/components/BottomMenu/index.tsx
+++ src/components/BottomMenu/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Block from '@components/Block'
-import Text from '@components/Text'
+import Block from '@/components/Block'
+import Text from '@/components/Text'
 
 import { useNavigate } from 'react-router-dom'
 
