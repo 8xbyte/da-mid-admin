09bd04c1e73193cfe512add61e08e9a41ac47539
micmaclaynd <101046221+micmaclaynd@users.noreply.github.com>
2024-08-10 15:45:15 +0300

    Update audiences page Add search Update styles

--- src/pages/AudiencesPage/index.tsx
+++ src/pages/AudiencesPage/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Block from '@/components/Block'
+import Search from '@/components/Search'
 
 import * as styles from './audiences-page.module.scss'
 
 const AudiencesPage: React.FC = () => {
-    return <Block className={styles.audiencesPage}>AudiencesPage</Block>
+    return (
+        <Block className={styles.audiencesPage}>
+            <Search placeholder='Поиск'/>
+        </Block>
+    )
 }
 
 export default AudiencesPage
