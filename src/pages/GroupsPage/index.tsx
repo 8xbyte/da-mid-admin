4f1d1177082870bd82f5491c18df12dc46a828c1
micmaclaynd <101046221+micmaclaynd@users.noreply.github.com>
2024-08-10 15:45:28 +0300

    Update groups page Add search Update styles

--- src/pages/GroupsPage/index.tsx
+++ src/pages/GroupsPage/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Block from '@/components/Block'
+import Search from '@/components/Search'
 
 import * as styles from './groups-page.module.scss'
 
 const GroupsPage: React.FC = () => {
-    return <Block className={styles.groupsPage}>GroupsPage</Block>
+    return (
+        <Block className={styles.groupsPage}>
+            <Search placeholder='Поиск' />
+        </Block>
+    )
 }
 
 export default GroupsPage
