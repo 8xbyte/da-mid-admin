aced7699d142db0eecc85d2482d5e3ebd5fb140a
micmaclaynd <101046221+micmaclaynd@users.noreply.github.com>
2024-08-10 15:45:49 +0300

    Update teachers page Add search Update styles

--- src/pages/TeachersPage/index.tsx
+++ src/pages/TeachersPage/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Block from '@/components/Block'
+import Search from '@/components/Search'
 
 import * as styles from './teachers-page.module.scss'
 
 const TeachersPage: React.FC = () => {
-    return <Block className={styles.teachersPage}>TeachersPage</Block>
+    return (
+        <Block className={styles.teachersPage}>
+            <Search placeholder='Поиск' />
+        </Block>
+    )
 }
 
 export default TeachersPage
