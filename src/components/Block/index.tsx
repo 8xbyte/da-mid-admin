778e4d318463a50523442ebeb7ed82d2cdd61ebb
micmaclaynd <101046221+micmaclaynd@users.noreply.github.com>
2024-08-10 14:47:46 +0300

    Update block component Rename props interface Now export props interface

--- src/components/Block/index.tsx
+++ src/components/Block/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
+
 import * as styles from './block.module.scss'
 
-interface IProps
+export interface IBlockProps
     extends React.DetailedHTMLProps<
         React.HTMLAttributes<HTMLDivElement>,
         HTMLDivElement
     > {}
 
-const Block: React.FC<IProps> = ({ className, children, ...others }) => {
+const Block: React.FC<IBlockProps> = ({ className, children, ...others }) => {
     return (
         <div className={[styles.defaultBlock, className].join(' ')} {...others}>
             {children}
