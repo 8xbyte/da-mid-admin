e34518f25ea6e3f9669a896a1cb65d21d751f0e6
micmaclaynd <101046221+micmaclaynd@users.noreply.github.com>
2024-08-10 15:45:39 +0300

    Update subjects page Add search Update styles

--- src/pages/SubjectsPage/index.tsx
+++ src/pages/SubjectsPage/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Block from '@/components/Block'
+import Search from '@/components/Search'
 
 import * as styles from './subjects-page.module.scss'
 
 const SubjectsPage: React.FC = () => {
-    return <Block className={styles.subjectsPage}>SubjectsPage</Block>
+    return (
+        <Block className={styles.subjectsPage}>
+            <Search placeholder='Поиск' />
+        </Block>
+    )
 }
 
 export default SubjectsPage
