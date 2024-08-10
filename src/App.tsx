5239afce923345e2cf2e9d415a605c42aea5915b
micmaclaynd <101046221+micmaclaynd@users.noreply.github.com>
2024-08-10 14:08:18 +0300

    Update app component Add bottom menu in layout Add redirect for unused paths

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,23 @@ import ClassesPage from '@pages/ClassesPage'
 import GroupsPage from '@pages/GroupsPage'
 import SubjectsPage from '@pages/SubjectsPage'
 import TeachersPage from '@pages/TeachersPage'
+import BottomMenu from './components/BottomMenu'
 
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 
 import * as styles from './app.module.scss'
 
 const App: React.FC = () => {
     return (
         <Block className={styles.app}>
+            <BottomMenu />
             <Routes>
                 <Route path='/audiences' element={<AudiencesPage />} />
                 <Route path='/classes' element={<ClassesPage />} />
                 <Route path='/groups' element={<GroupsPage />} />
                 <Route path='/subjects' element={<SubjectsPage />} />
                 <Route path='/teachers' element={<TeachersPage />} />
+                <Route path='*' element={<Navigate to='/audiences' />} />
             </Routes>
         </Block>
     )
