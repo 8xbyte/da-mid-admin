03020fd2f6eef79a0593bc20ec229ed9bd4a002a
micmaclaynd <101046221+micmaclaynd@users.noreply.github.com>
2024-08-12 21:50:41 +0300

    Update audiences slice Add search state

--- src/store/slices/audiencesSlice.ts
+++ src/store/slices/audiencesSlice.ts
@@ -1,10 +1,20 @@
-import { addAudience, getAudiences, removeAudience } from '@/api/audiences'
-import { IApiEmptyResult, IApiResult, IApiStatus } from '@/interfaces/api'
+import {
+    addAudience,
+    getAudiences,
+    removeAudience,
+    searchAudiences
+} from '@/api/audiences'
+import { IApiStatus } from '@/interfaces/api'
 import { IAudience } from '@/interfaces/audiences'
 import { createSlice } from '@reduxjs/toolkit'
 
 interface IInitialState {
     get: {
+        status: IApiStatus | null
+        error: string | null
+        result: Array<IAudience> | null
+    }
+    search: {
         status: IApiStatus | null
         error: string | null
         result: Array<IAudience> | null
@@ -23,6 +33,11 @@ interface IInitialState {
 
 const initialState: IInitialState = {
     get: {
+        status: null,
+        error: null,
+        result: null
+    },
+    search: {
         status: null,
         error: null,
         result: null
@@ -58,6 +73,19 @@ const audiencesSlice = createSlice({
             })
 
         builder
+            .addCase(searchAudiences.pending, (state, action) => {
+                state.search.status = 'loading'
+            })
+            .addCase(searchAudiences.rejected, (state, action) => {
+                state.search.status = 'failed'
+                state.search.error = action.payload as string
+            })
+            .addCase(searchAudiences.fulfilled, (state, action) => {
+                state.search.status = 'success'
+                state.search.result = action.payload.result
+            })
+
+        builder
             .addCase(addAudience.pending, (state, action) => {
                 state.add.status = 'loading'
             })
@@ -72,15 +100,15 @@ const audiencesSlice = createSlice({
 
         builder
             .addCase(removeAudience.pending, (state, action) => {
-                state.add.status = 'loading'
+                state.remove.status = 'loading'
             })
             .addCase(removeAudience.rejected, (state, action) => {
-                state.add.status = 'failed'
-                state.add.error = action.payload as string
+                state.remove.status = 'failed'
+                state.remove.error = action.payload as string
             })
             .addCase(removeAudience.fulfilled, (state, action) => {
-                state.add.status = 'success'
-                state.add.result = action.payload.status
+                state.remove.status = 'success'
+                state.remove.result = action.payload.status
             })
     }
 })
