40101d808034eea0a557aca851f03cf35d790c43
micmaclaynd <101046221+micmaclaynd@users.noreply.github.com>
2024-08-12 13:23:20 +0300

    Update app component Add login page in layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,51 @@
-import React from 'react'
 import Block from '@/components/Block'
+import BottomMenu from '@/components/BottomMenu'
 import AudiencesPage from '@/pages/AudiencesPage'
 import ClassesPage from '@/pages/ClassesPage'
 import GroupsPage from '@/pages/GroupsPage'
 import SubjectsPage from '@/pages/SubjectsPage'
 import TeachersPage from '@/pages/TeachersPage'
-import BottomMenu from '@/components/BottomMenu'
+import React from 'react'
+import LoginPage from './pages/LoginPage'
 
-import { Navigate, Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes, useNavigate } from 'react-router-dom'
+import { useAppSelector } from './store/store'
 
 import * as styles from './app.module.scss'
 
 const App: React.FC = () => {
+    const auth = useAppSelector((state) => state.auth)
+
+    const navigate = useNavigate()
+
+    React.useEffect(() => {
+        if (auth.isAuth) {
+            navigate('/audiences')
+        } else {
+            navigate('/login')
+        }
+    }, [auth.isAuth])
+
     return (
         <Block className={styles.app}>
-            <BottomMenu />
-            <Block className={styles.page}>
-                <Routes>
-                    <Route path='/audiences' element={<AudiencesPage />} />
-                    <Route path='/classes' element={<ClassesPage />} />
-                    <Route path='/groups' element={<GroupsPage />} />
-                    <Route path='/subjects' element={<SubjectsPage />} />
-                    <Route path='/teachers' element={<TeachersPage />} />
-                    <Route path='*' element={<Navigate to='/audiences' />} />
-                </Routes>
-            </Block>
+            {auth.isAuth ? (
+                <Block className={styles.page}>
+                    <BottomMenu />
+                    <Routes>
+                        <Route path='/audiences' element={<AudiencesPage />} />
+                        <Route path='/classes' element={<ClassesPage />} />
+                        <Route path='/groups' element={<GroupsPage />} />
+                        <Route path='/subjects' element={<SubjectsPage />} />
+                        <Route path='/teachers' element={<TeachersPage />} />
+                        <Route
+                            path='*'
+                            element={<Navigate to='/audiences' />}
+                        />
+                    </Routes>
+                </Block>
+            ) : (
+                <LoginPage />
+            )}
         </Block>
     )
 }
