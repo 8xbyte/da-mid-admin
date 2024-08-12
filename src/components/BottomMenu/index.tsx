82ee4439b5d249d00d06bc9f6c4c14a23936b025
micmaclaynd <101046221+micmaclaynd@users.noreply.github.com>
2024-08-12 13:19:50 +0300

    Update bottom menu component Change block to shadow block Update styles

--- src/components/BottomMenu/index.tsx
+++ src/components/BottomMenu/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Block from '@/components/Block'
 import Text from '@/components/Text'
+import ShadowBlock from '../ShadowBlock'
 
 import { useNavigate } from 'react-router-dom'
 
@@ -10,16 +11,41 @@ const BottomMenu: React.FC = () => {
     const navigate = useNavigate()
 
     return (
-        <Block className={styles.bottomMenu}>
+        <ShadowBlock className={styles.bottomMenu}>
             <Block className={styles.topMenu}>
-                <Text onClick={() => navigate('/audiences')} className={styles.menuItem}>Аудитории</Text>
-                <Text onClick={() => navigate('/subjects')} className={styles.menuItem}>Предметы</Text>
-                <Text onClick={() => navigate('/groups')} className={styles.menuItem}>Группы</Text>
-                <Text onClick={() => navigate('/teachers')} className={styles.menuItem}>Преподаватели</Text>
-                <Text onClick={() => navigate('/classes')} className={styles.menuItem}>Занятия</Text>
+                <Text
+                    onClick={() => navigate('/audiences')}
+                    className={styles.menuItem}
+                >
+                    Аудитории
+                </Text>
+                <Text
+                    onClick={() => navigate('/subjects')}
+                    className={styles.menuItem}
+                >
+                    Предметы
+                </Text>
+                <Text
+                    onClick={() => navigate('/groups')}
+                    className={styles.menuItem}
+                >
+                    Группы
+                </Text>
+                <Text
+                    onClick={() => navigate('/teachers')}
+                    className={styles.menuItem}
+                >
+                    Преподаватели
+                </Text>
+                <Text
+                    onClick={() => navigate('/classes')}
+                    className={styles.menuItem}
+                >
+                    Занятия
+                </Text>
             </Block>
             <Text className={styles.menuItem}>Выход</Text>
-        </Block>
+        </ShadowBlock>
     )
 }
 
